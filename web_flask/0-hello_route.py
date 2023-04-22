5b4ab4a241a89a1cfafe5b8ff32838e614631557
KipEric <kipkoecheric97@gmail.com>
2023-04-22 07:28:54 +0300

    Task 0 correction 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-"""Function that return Hello HBNB"""
-def hello:
+#Function that return Hello HBNB
+def hello():
     return "Hello HBNB"
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
