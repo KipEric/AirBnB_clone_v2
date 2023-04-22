2b0a079057410666b2f11fc24c4c1e97a220a5fa
KipEric <kipkoecheric97@gmail.com>
2023-04-22 08:23:46 +0300

    Task 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+""" Function that return Hello HBNB"""
 @app.route('/', strict_slashes=False)
-#Function that return Hello HBNB
 def hello():
     return "Hello HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
-""" Function that return hello HBNB text"""
+
+"""Function that retun hello hbnb"""
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
 
-"""Function that return HBNB text"""
+"""Function that return HBNB"""
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
+
+"""Function that return c text"""
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
