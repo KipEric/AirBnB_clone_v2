9b3ce6dcb56074ae1f2efeffbe9e18a291ac94b7
KipEric <kipkoecheric97@gmail.com>
2023-05-17 18:11:53 +0300

    Task 0 corr

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 """ Function that return Hello HBNB"""
 @app.route('/', strict_slashes=False)
 def hello():
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
